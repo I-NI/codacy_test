2fb078eb1ba4e9e3056f7283030a551dc7397fc2
I-NI <240657951@qq.com>
2018-11-15 19:36:18 +0800

    Add files via upload

---
+++ laser.ino
@@ -0,0 +1,79 @@
+/*车辆内轮差预警装置从机
+ * 接收主机传来的转角数据，如果转弯，光线预警启动，根据内轮差，由舵机控制遮蔽物控制投影范围。
+ * ini
+ * 最后一次更改于2018/6/1
+ */
+
+float comfloat;
+double innerdistence=0,humandistence=0;//定义内轮差及与行人距离。 
+double car_length=0.5;//定义车长
+double pos=0;//定义舵机转角，单位度
+      int relay_pin=5;//定义继电器针脚为A4
+int servopin=10;//定义数字接口9 连接伺服舵机信号线
+
+int myangle;//定义角度变量
+int pulsewidth;//定义脉宽变量
+int val;
+
+void servopulse(int servopin,int myangle)//定义一个脉冲函数
+{
+  pulsewidth=(myangle*11)+500;//将角度转化为500-2480 的脉宽值
+  digitalWrite(servopin,HIGH);//将舵机接口电平至高
+  delayMicroseconds(pulsewidth);//延时脉宽值的微秒数
+  digitalWrite(servopin,LOW);//将舵机接口电平至低
+  delay(20-pulsewidth/1000);
+}
+//定义电位器测定转角函数
+double potentionmeter()
+{
+    innerdistence=car_length*tan(0.5*comfloat);//根据数学模型得到内轮差
+    return(innerdistence);//返回内轮差
+  }
+
+//定义光线预警函数
+ void projection(double innerdistence)
+{
+   digitalWrite(relay_pin,LOW);//继电器断开
+   if (innerdistence>0.01  )//如果转弯
+   {
+     digitalWrite(relay_pin,HIGH);//继电器接上
+     pos=atan(1.5*innerdistence/car_length);//根据数学模型算出舵机转角
+     pos=pos/PI*180;
+    }
+  else
+  {
+    pos=0;
+  }
+  for(int i=0;i<=5;i++) //给予舵机足够的时间让它转到指定角度
+  {
+    servopulse(servopin,pos+10);//引用脉冲函数
+  }
+}
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(38400);//设置波特率
+  while(Serial.read()>= 0){}
+  pinMode(relay_pin,OUTPUT);
+  pinMode(servopin,OUTPUT);//设定舵机接口为输出接口
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float coming;
+  delay(100);
+  if(Serial.available()>0){
+      coming = Serial.parseFloat();//接收主机传来的角度数据
+      if(coming<1.1 && coming>0.09)
+      {
+         comfloat=coming-0.1;
+         //Serial.print("Serial.parseFloat:");
+         Serial.println(comfloat);
+         innerdistence=potentionmeter();//电位器测定车轮转角,计算内轮差
+      }
+      projection(innerdistence); 
+    }
+  while(Serial.read() >= 0){}
+  }
+
+
+
---
+++ radar.ino
@@ -0,0 +1,107 @@
+/*车辆内轮差预警装置主机
+ * 获取车轮转角数据，并传给从机，如果转弯，雷达转动，判断障碍物位置，计算内轮差危险区域范围，如果危险区域内有行人，则蜂鸣器报警。
+ * ini
+ * 最后一次更改于2018/6/3
+ */
+
+#include<Servo.h>
+
+#define PIN_SERVO 10
+Servo myservo;
+
+int mAngleNum=20;//当前角度
+double hudu;
+char mFront=0;//正转反转
+
+const int TrigPin = 2;//超声波引脚 
+const int EchoPin = 3;
+int relay_pin=5;//定义继电器针脚为A4
+int buzzerPin=7;//蜂鸣器针脚为7
+
+float distance,x,y;
+double innerdistence=0,inner_human=-1,humandistence=100,val=0,degree=0;//定义内轮差及与行人距离。 
+double wheel_length=45;//定义轴距cm
+double car_length=50;//定义车长cm
+int i=0;
+
+double ultrasonic(void)//超声波测距
+          {
+            // 产生一个10us的高脉冲去触发TrigPin 
+        digitalWrite(TrigPin, LOW); 
+        delayMicroseconds(2); 
+        digitalWrite(TrigPin, HIGH); 
+        delayMicroseconds(10);
+        digitalWrite(TrigPin, LOW); 
+    // 检测脉冲宽度，并计算出距离
+        distance = pulseIn(EchoPin, HIGH) / 58.00;//!!!
+        //Serial.println(distance); //cm
+        //delay(10); 
+        return(distance);
+          }
+          
+ //定义电位器测定转角函数
+double potentionmeter(double Length)
+{
+    int potentionmeter_pin=A0;//定义电位器针脚为A0
+    double degree,innerdistence;//定义电位器传回值，角度，内轮差
+    val = analogRead(potentionmeter_pin);//接收电位器传回值（0到1024）
+   // Serial.println(val);
+    degree=val*PI/1024;//转角为0到1.5pi，转换得角度
+    if(i>10){
+    Serial.println(degree+0.1);
+    i=0;
+    }
+    i++;
+    //delay(10);
+    innerdistence=Length*tan(0.5*degree);//根据数学模型得到内轮差
+    //Serial.println(innerdistence);
+    return(innerdistence);//返回内轮差
+  }
+  //定义声音预警函数
+    void buzzer(double innerdistence,double humandistence)
+    {
+       if(innerdistence>humandistence)//单位转换
+        tone(buzzerPin,400);
+      else
+        noTone(buzzerPin);
+      }
+      
+void setup()
+{
+    Serial.begin(38400);
+    myservo.attach(PIN_SERVO);
+    pinMode(TrigPin, OUTPUT); 
+    pinMode(EchoPin, INPUT);
+    pinMode(buzzerPin,OUTPUT); 
+    pinMode(relay_pin,OUTPUT);
+  } 
+
+void loop()
+{
+  innerdistence=potentionmeter(wheel_length);//电位器测定车轮转角,计算内轮差
+  if(innerdistence>1)//内轮差到一个阈值
+  {
+    if(innerdistence<humandistence)//如果危险区域内无行人
+    {
+      myservo.write(180-mAngleNum);//舵机继续转
+      if(mFront==0)
+      {
+        mAngleNum++;
+        if(mAngleNum>160)
+          mFront=1;
+        }
+        else
+        {
+          mAngleNum--;
+          if(mAngleNum<20)
+          mFront=0;
+          }
+    }
+        humandistence=ultrasonic();//超声波测距
+        hudu=mAngleNum*3.14/180;//根据转角判断行人相对于车的具体位置
+        y=distance*cos(hudu);//cm
+        x=distance*sin(hudu);//cm 
+        buzzer(innerdistence,humandistence);//报警
+  }
+}
+
